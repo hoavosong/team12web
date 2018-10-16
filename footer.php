f27fdb13ba8c5d70ce86aab75ed37e615c6aa563
hoavosong <37056599+hoavosong@users.noreply.github.com>
2018-10-16 16:33:40 +0700

    Delete footer.php

--- footer.php
+++
@@ -1,9 +0,0 @@
-<script src="jquery.js"></script>
-<script src="bootstrap.min.js"></script>
-</body>
-<footer>
-  <form style="height:100px;width: 100%;background-color:rgb(160, 166, 96);" >
-  </form>
-</footer>
-
-</html>
